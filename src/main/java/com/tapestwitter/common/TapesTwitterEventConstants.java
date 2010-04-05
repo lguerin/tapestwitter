d1d9fe499585e13d98be414b3ede977a89a3327b
lguerin <laurent@dreamcat.fr>
2010-04-06 00:28:31 +0200

    Integration of AjaxMoreResults component

---
+++ src/main/java/com/tapestwitter/common/TapesTwitterEventConstants.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.tapestwitter.common;
+
+import com.tapestwitter.components.AjaxMoreResults;
+
+/**
+ * 
+ * This class contains the events triggereds by the TapesTwitter applications.
+ * 
+ * @author lguerin
+ *
+ */
+public class TapesTwitterEventConstants
+{
+	/**
+	 * Event triggered when the {@link AjaxMoreResults} component link is 
+	 * clicked.
+	 */
+	public static final String PROVIDE_MORE_RESULTS = "provideMoreResults";
+}
