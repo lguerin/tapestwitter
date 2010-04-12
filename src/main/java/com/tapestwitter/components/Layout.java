2fbf66539f5793570f31ef1a1c2c4e7e4efff400
Laurent Guerin <laurent@dreamcat.fr>
2010-04-12 16:08:59 +0200

    Fix authentification implementation error

--- src/main/java/com/tapestwitter/components/Layout.java
+++ src/main/java/com/tapestwitter/components/Layout.java
@@ -1,14 +1,19 @@
 package com.tapestwitter.components;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import com.tapestwitter.services.TapestwiterURLResolver;
 import com.tapestwitter.services.security.TapestwitterSecurityContext;
 
+import org.slf4j.Logger;
+
 import org.apache.tapestry5.BindingConstants;
-import org.apache.tapestry5.annotations.Log;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
 
 /**
  * Main Layout component for pages of application tapestwitter.
@@ -32,10 +37,27 @@ public class Layout
 		return urlResolver.getLogoutUrl();
 	}
 
-	@Log
+	@Inject
+	private Request request;
+
+	@Inject
+	private Logger logger;
+
 	@OnEvent(component = "logout")
-	public void onLogout()
+	public Object onLogout()
 	{
+		logger.debug(">>> invalidate the current user session");
+		request.getSession(false).invalidate();
 		securityContext.logout();
+		URL external = null;
+		try
+		{
+			external = new URL("http://localhost:8080/tapestwitter/j_spring_security_logout");
+		}
+		catch (MalformedURLException e)
+		{
+			throw new RuntimeException(e);
+		}
+		return external;
 	}
 }
