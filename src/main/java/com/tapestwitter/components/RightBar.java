5dfdf556b0354555d40ac57f1d9ece4ca5661438
Laurent Guerin <laurent@dreamcat.fr>
2010-04-12 11:06:19 +0200

    Add RightBar component

---
+++ src/main/java/com/tapestwitter/components/RightBar.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.tapestwitter.components;
+
+import com.tapestwitter.domain.business.TweetManager;
+import com.tapestwitter.domain.model.User;
+import com.tapestwitter.services.security.TapestwitterSecurityContext;
+
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+/**
+ * The rightbar menu component
+ * @author lguerin
+ *
+ */
+public class RightBar
+{
+	@Inject
+	private TapestwitterSecurityContext securityContext;
+
+	@Property(write = false)
+	private User user = securityContext.getUser();
+
+	@Inject
+	private TweetManager tweetManager;
+
+	@SuppressWarnings("unused")
+	@Property(write = false)
+	private Integer userNbTweets;
+
+	@SetupRender
+	public void setup()
+	{
+		userNbTweets = tweetManager.getNbTweetsByUser(user.getLogin());
+	}
+}
