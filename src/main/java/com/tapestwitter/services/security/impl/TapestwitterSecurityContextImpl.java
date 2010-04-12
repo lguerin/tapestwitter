f39754732288aa7b70965cb5c414c51c7b40e3c3
Laurent Guerin <laurent@dreamcat.fr>
2010-04-12 16:13:21 +0200

    Fix authentification implementation error

--- src/main/java/com/tapestwitter/services/security/impl/TapestwitterSecurityContextImpl.java
+++ src/main/java/com/tapestwitter/services/security/impl/TapestwitterSecurityContextImpl.java
@@ -3,15 +3,10 @@ package com.tapestwitter.services.security.impl;
 import com.tapestwitter.domain.model.User;
 import com.tapestwitter.services.security.TapestwitterSecurityContext;
 
-import org.slf4j.Logger;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
-
-import org.apache.tapestry5.annotations.Log;
-import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.services.Request;
 
 /**
  * Implémentation web du TapestwitterSecurityContext
@@ -22,12 +17,6 @@ import org.apache.tapestry5.services.Request;
 @Component("tapestwitterSecurityContext")
 public class TapestwitterSecurityContextImpl implements TapestwitterSecurityContext
 {
-	@Inject
-	private Logger logger;
-
-	@Inject
-	private Request request;
-
 	/**
 	 * La méthode log in l'utilisateur
 	 * Le TOKEN est mis sur le security context
@@ -68,12 +57,8 @@ public class TapestwitterSecurityContextImpl implements TapestwitterSecurityCont
 		return user;
 	}
 
-	@Log
 	public void logout()
 	{
-		// Invalidate the current Tapestry user session
-		//logger.info("Invalidate the Tapestry session of the current user: " + getUser().getLogin());
-		//Session currentSession = request.getSession(true);
 		SecurityContextHolder.getContext().setAuthentication(null);
 		SecurityContextHolder.clearContext();
 	}
