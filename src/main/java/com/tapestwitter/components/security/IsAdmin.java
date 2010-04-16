327888a185e102a7aa8c2bb2a545e288ad0092c8
Katia Aresti <katiaaresti@gmail.com>
2010-04-16 16:12:37 +0200

    Administrator
    RigthBar with Admonistrator

---
+++ src/main/java/com/tapestwitter/components/security/IsAdmin.java
@@ -0,0 +1,20 @@
+package com.tapestwitter.components.security;
+
+import com.tapestwitter.services.security.TapestwitterSecurityContext;
+
+import org.apache.tapestry5.corelib.base.AbstractConditional;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+public class IsAdmin extends AbstractConditional
+{
+
+	@Inject
+	private TapestwitterSecurityContext securityCtx;
+
+	@Override
+	protected boolean test()
+	{
+		return securityCtx.getUser() != null && securityCtx.getUser().isAdmin();
+	}
+
+}
