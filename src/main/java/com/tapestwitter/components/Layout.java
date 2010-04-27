951554f73982d7ccdf9905682c8063a6da1a55b3
Katia Aresti <katiaaresti@gmail.com>
2010-04-27 23:35:43 +0200

    Refactoring into a SimpleLayout
    Signup and Index pages have a different Layout, new component SimpleLayout

--- src/main/java/com/tapestwitter/components/Layout.java
+++ src/main/java/com/tapestwitter/components/Layout.java
@@ -1,26 +1,16 @@
 package com.tapestwitter.components;
+
 import com.tapestwitter.services.TapestwiterURLResolver;
 
-import org.apache.tapestry5.BindingConstants;
-import org.apache.tapestry5.annotations.Parameter;
-import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
-
 
 /**
  * Main Layout component for pages of application tapestwitter.
  */
-public class Layout
+public class Layout extends SimpleLayout
 {
-	/** The page title, for the <title> element and the <h1> element. */
-	@SuppressWarnings("unused")
-	@Property
-	@Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
-	private String title;
-
 	@Inject
 	private TapestwiterURLResolver urlResolver;
-
 
 	public String getLogoutUrl()
 	{
