3fa56a3cbaf31226d4fb7f0c05de37d02f95d8c8
Laurent Guerin <laurent@dreamcat.fr>
2011-06-14 19:32:04 +0200

    Implementation of isTweetOwner component

--- src/main/java/com/tapestwitter/components/security/IsTweetOwner.java
+++ src/main/java/com/tapestwitter/components/security/IsTweetOwner.java
@@ -1,6 +1,12 @@
 package com.tapestwitter.components.security;
 
+import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.corelib.base.AbstractConditional;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import com.tapestwitter.domain.business.TweetLoader;
+import com.tapestwitter.domain.model.Tweet;
+import com.tapestwitter.services.security.SecurityContext;
 
 /**
  * This component verifies that the current
@@ -11,9 +17,20 @@ import org.apache.tapestry5.corelib.base.AbstractConditional;
 public class IsTweetOwner extends AbstractConditional
 {
 
+    @Inject
+    private ComponentResources resources;
+
+    @Inject
+    private TweetLoader tweetLoader;
+
+    @Inject
+    private SecurityContext securityCtx;
+
     @Override
     protected boolean test()
     {
-        return true;
+        String tweetId = resources.getInformalParameter("tweetId", String.class);
+        Tweet current = tweetLoader.findTweetById(new Long(tweetId));
+        return securityCtx.getUser() != null && securityCtx.getUser().getLogin().equals(current.getAuthor());
     }
 }
